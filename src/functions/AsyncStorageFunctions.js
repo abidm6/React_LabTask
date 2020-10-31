ad41acf338ae6e359becda6b64f9732de59b7488
abidm6 <muhtasimabid@iut-dhaka.edu>
2020-10-31 20:49:05 +0600

    First Commit

---
+++ src/functions/AsyncStorageFunctions.js
@@ -0,0 +1,49 @@
+import { AsyncStorage } from "react-native";
+
+
+const storeDataJSON = async (key, value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem(key, jsonValue);
+    alert("Data Stored Successfully!");
+  } catch (error) {
+    alert(error);
+  }
+};
+
+const getData = async (key) => {
+  try {
+    let data = await AsyncStorage.getItem(key);
+    if (data != null) {
+      return data;
+    } else {
+      alert("No data with this key!");
+    }
+  } catch (error) {
+    alert(error);
+  }
+};
+const getDataJSON = async (key) => {
+  try {
+    let data = await AsyncStorage.getItem(key);
+    if (data != null) {
+      const jsonData = JSON.parse(data);
+      return jsonData;
+    } else {
+      alert("No data with this key!");
+    }
+  } catch (error) {
+    alert(error);
+  }
+};
+
+const removeData = async (key) => {
+  try {
+    await AsyncStorage.removeItem(key);
+    alert("Data Removed Successfully");
+  } catch (error) {
+    alert(error);
+  }
+};
+
+export { storeDataJSON, getData, getDataJSON, removeData };
---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { View, StyleSheet, AsyncStorage } from "react-native";
+import { Text, Card, Button, Avatar, Header } from "react-native-elements";
+import { AuthContext } from "../providers/AuthProvider";
+const ProfileScreen = (props) => {
+  return (
+    <AuthContext.Consumer>
+      {(auth) => (
+        <View style={styles.viewStyle}>
+          <Header
+            leftComponent={{
+              icon: "menu",
+              color: "#fff",
+              onPress: function () {
+                props.navigation.toggleDrawer();
+              },
+            }}
+            centerComponent={{ text: "The Office", style: { color: "#fff" } }}
+            rightComponent={{
+              icon: "lock-outline",
+              color: "#fff",
+              onPress: function () {
+                auth.setIsLoggedIn(false);
+                auth.setCurrentUser({});
+              },
+            }}
+          />
+          <Card>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Text style={{ paddingHorizontal: 10 }}>
+                Name: {auth.CurrentUser.name}
+              </Text>
+            </View>
+          </Card>
+          <Card>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Text style={{ paddingHorizontal: 10 }}>
+                Born On: {auth.CurrentUser.date}
+              </Text>
+            </View>
+          </Card>
+        </View>
+      )}
+    </AuthContext.Consumer>
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 30,
+    color: "blue",
+  },
+  viewStyle: {
+    flex: 1,
+  },
+});
+
+export default ProfileScreen;
